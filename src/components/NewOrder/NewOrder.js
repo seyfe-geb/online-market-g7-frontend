2b92147852c4a41aea8cbe5878f170e9a9bff470
seyfe-geb <syberglife@gmail.com>
2022-04-18 14:36:42 -0500

    First Skeleton Push

---
+++ src/components/NewOrder/NewOrder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewOrder = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default NewOrder;
---
+++ src/pages/PageRoutes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageRoutes = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default PageRoutes;
