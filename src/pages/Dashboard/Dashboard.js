bba05d249e04a80094bd08cbc62acfafa229411f
smehari57 <starmehari5@gmail.com>
2022-04-18 20:23:32 -0500

    All product

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Users from "../../containers/Users/Users";
+import Products from "../../containers/Products/Products";
 
 const Dashboard = () => {
     return (
         <div>
             <Users/>
+            <Products/>
         </div>
     );
 };
