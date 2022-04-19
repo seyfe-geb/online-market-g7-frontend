ae2a7e28b8921fde64bb766f6876f1d73e976ff4
Ermias <eghebrez@gmail.com>
2022-04-18 19:50:04 -0500

    Reward files.

---
+++ src/components/Reward/Reward.js
@@ -0,0 +1,12 @@
+
+
+const Reward = (props) => {
+    return (
+        <div className="Reward">
+            <h1>{props.id}</h1>
+            <div className="Field">
+                {}
+            </div>
+        </div>
+    )
+}
